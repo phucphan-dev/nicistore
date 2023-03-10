a5ead803e8997d114e3d103ca762214ca4b57eef
PhucPhan-Dev <phanhongphuc.dev@gmail.com>
2023-03-10 09:26:39 +0700

    Setting Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import './App.scss';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import MainLayout from 'components/templates/MainLayout';
 import Home from 'pages/Home';
 import Products from 'pages/Products';
 
 const App: React.FC = () => (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
